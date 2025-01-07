d96f219c5c523ef956f4ba551248e0f88ffea123
ehsan tafehi <ehsan.tafehi@dnb.no>
2025-01-07 11:29:15 +0100

    Move stages to run consecutive

--- HuggingFace/aws/backend.tf
+++ HuggingFace/aws/backend.tf
@@ -1,20 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "terraform-state-bucket"
-#     encrypt = true
-#     key    = "sagemaker/terraform.tfstate"
-#     region = "eu-west-1"
-#     #    dynamodb_table = "terraform_backend_lock"
-#   }
-# }
-
 terraform {
   backend "s3" {
-    bucket  = "fvc-terraform-state-bucket-dev"
+    bucket  = "terraform-state-bucket-et"
     encrypt = true
-    #"allente-terrafrom-sandbox/logstash/" 
     key    = "sagemaker/terraform.tfstate"
     region = "eu-west-1"
     #    dynamodb_table = "terraform_backend_lock"
   }
 }
+
