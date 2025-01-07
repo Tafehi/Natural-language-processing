ea92ec6c3084881cfa4af4a0e6755cb6f05e4c75
ehsan tafehi <ehsan.tafehi@dnb.no>
2025-01-07 11:48:48 +0100

    modify the aws region to stockholm

--- HuggingFace/aws/backend.tf
+++ HuggingFace/aws/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket  = "terraform-state-bucket-et"
     encrypt = true
     key    = "sagemaker/terraform.tfstate"
-    region = "eu-west-1"
+    region = "eu-north-1"
     #    dynamodb_table = "terraform_backend_lock"
   }
 }
